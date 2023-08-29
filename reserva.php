f02f7e621ed7749b9a666d9010c7752d05d70761
igor cunha <igorcunha.melo1@gmail.com>
2023-08-29 12:41:54 -0300

    V2.1

--- reserva.php
+++ reserva.php
@@ -7,9 +7,8 @@ $dbname = "teste";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
-    var_dump('ConnectionError : ' . $conn->connect_error);
+    var_dump('ConnectionError: ' . $conn->connect_error);
 }
-
 
 // Processar o formulário de reserva
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -19,15 +18,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $reservation_date = $_POST["reservation_date"];
     $time = $_POST["time"];
     $chefe = $_POST["chefe"];
-    
 
-    // Inserir a reserva no banco de dados
-    $sql = "INSERT INTO reservations (car, quantidade, name, reservation_date, time, chefe) VALUES ('$car','$quantidade', '$name', '$reservation_date', '$time', '$chefe')";
-    if ($conn->query($sql) === TRUE) {
+    // Usar declaração preparada para inserir a reserva no banco de dados
+    $sql = "INSERT INTO reservations (car, quantidade, name, reservation_date, time, chefe) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssssss", $car, $quantidade, $name, $reservation_date, $time, $chefe);
+
+    if ($stmt->execute()) {
         echo "Reserva efetuada com sucesso!";
     } else {
-        echo "Erro ao efetuar reserva: " . $conn->error;
+        echo "Erro ao efetuar reserva: " . $stmt->error;
     }
+
+    $stmt->close();
 }
 
 $conn->close();
